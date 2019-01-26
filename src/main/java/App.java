d7eb61cdf7290c2d8edcce03ac750497d4bd90c5
Dun-Njuguna <duncankariuki60@gmail.com>
2019-01-26 15:57:57 +0300

    made changes to decoding to accept key

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         Encoding encoding = new Encoding("","",22,"");
 
-
 // encoding
+
         System.out.println("Add your string");
         String userString = scanner.nextLine();
         encoding.setMinput(userString);
         encoding.encrypt(userString,22);
+        System.out.println("Input: " + encoding.getMinput() );
         System.out.println("Encoded: " + encoding.getResult());
+
 // decoding
+
         Decoding decoding = new Decoding("","");
         String encoded = encoding.getResult();
         decoding.setEncoded(encoded);
